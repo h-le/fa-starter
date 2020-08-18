08c99344bde6b61f7a5d2f6133e13933dd0b64aa
Henry Le <hle@raybeam.com>
2020-08-18 14:08:36 -0700

    User-level authorization flow as exercise; will eventually remove and use client access token

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 """Angular+Flask AppEnginer Starter App"""
 import os
 
+import json
 import flask
 import flask_cors
 from dotenv import load_dotenv
+from rauth import OAuth2Service
 
 import example
+
+CLIENT_ID = os.environ.get('GENIUS_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('GENIUS_CLIENT_SECRET')
+
+AUTHORIZE_URL = 'https://api.genius.com/oauth/authorize'
+ACCESS_TOKEN_URL = 'https://api.genius.com/oauth/token'
+BASE_URL = 'https://api.genius.com/'
+
+# TODO Get/set this another way
+REDIRECT_URI = 'http://localhost:5000/in'
 
 load_dotenv()
 
@@ -32,6 +44,10 @@ else:
 # Set the secret key to enable access to session data.
 app.secret_key = os.urandom(24)
 
+# TODO Remove when removing user-level auth code
+def oauth_decode(data):
+    """Decode UTF-8 and JSON"""
+    return json.loads(data.decode('utf-8', 'strict'))
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -49,3 +65,57 @@ def serve_angular(path):
         ])
         return flask.redirect(target)
     return flask.send_file('dist/client/index.html')
+
+@app.route('/login')
+def login():
+    """'Login' page for auth flow"""
+    return '<a href="/auth">Authorize</a>'
+
+@app.route('/auth')
+def oauth_flow():
+    """Redirect for user authorization"""
+    genius = OAuth2Service(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        name='genius',
+        authorize_url=AUTHORIZE_URL,
+        access_token_url=ACCESS_TOKEN_URL,
+        base_url=BASE_URL)
+
+    redirect_uri = REDIRECT_URI
+
+    # TODO Define a better state
+    state = 'state'
+
+    params = {
+        'scope': 'me',
+        'response_type': 'code',
+        'redirect_uri': redirect_uri,
+        'state': state}
+
+    authorize_url = genius.get_authorize_url(**params)
+
+    return flask.redirect(authorize_url)
+
+@app.route('/in')
+def auth_redirect():
+    """_Authorization successful_; display song lyrics to test acces token"""
+    genius = OAuth2Service(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        name='genius',
+        authorize_url=AUTHORIZE_URL,
+        access_token_url=ACCESS_TOKEN_URL,
+        base_url=BASE_URL)
+
+    session = genius.get_auth_session(
+        data={
+            'code': flask.request.args.get('code'),
+            'redirect_uri': REDIRECT_URI,
+            'grant_type': 'authorization_code'},
+        decoder=oauth_decode)
+
+    response = session.get(
+        'songs/{id}'.format(id=1929408))
+
+    return response.json()['response']['song']['embed_content']
