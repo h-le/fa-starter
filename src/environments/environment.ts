9e076e6eca7fed80850a6f853a0500ccd3424921
Henry Le <hle@raybeam.com>
2020-09-04 14:50:28 -0700

    Adding notes for local development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,16 @@
+/* 1. cp environment.ts environment.prod.ts */
+/* 2. Set up variables as necessary. */
+/* When working locally, `ng serve --prod` */
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
+  },
 };
