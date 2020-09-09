ade217b15adc7f39845f6bb2a8c62eb79d4a185d
Henry Le <hle@raybeam.com>
2020-09-09 11:18:06 -0700

    Initial unit-teesting setup

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,11 +1,26 @@
 import {TestBed} from '@angular/core/testing';
 import {AuthService} from './auth.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+      ],
+      providers: [AuthService],
+    });
     service = TestBed.inject(AuthService);
   });
 
--- src/app/signin/signin.component.spec.ts
+++ src/app/signin/signin.component.spec.ts
@@ -1,20 +1,32 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {SignInComponent} from './signin.component';
+import {AuthService} from '../auth.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../../environments/environment';
 
-import { SigninComponent } from './signin.component';
-
-describe('SigninComponent', () => {
-  let component: SigninComponent;
-  let fixture: ComponentFixture<SigninComponent>;
+describe('SignInComponent', () => {
+  let component: SignInComponent;
+  let fixture: ComponentFixture<SignInComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SigninComponent ]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+      ],
+      declarations: [SignInComponent],
+      providers: [AuthService],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SigninComponent);
+    fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
