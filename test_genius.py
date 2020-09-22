d95a92d5730beb7738491cdb2f0808cd78975102
Henry Le <hle@raybeam.com>
2020-09-22 14:18:38 -0700

    Using @patch.object, moved variables to setUp, and setUp no longer a @classmethod

--- test_genius.py
+++ test_genius.py
@@ -1,4 +1,5 @@
 """Tests for genius.py"""
+import os
 from unittest.mock import patch
 from dotenv import load_dotenv
 from absl.testing import absltest # pylint: disable=no-name-in-module
@@ -6,17 +7,14 @@ from utilities import genius
 
 class TestGenius(absltest.TestCase):
     """Genius Testing Class"""
-    @classmethod
-    def setUp(cls): # pylint: disable=invalid-name
+    def setUp(self): # pylint: disable=invalid-name
         """Set-up
             * Load environment variables from .env
+            * Mock song ID, song,(Genius) access token, headers, and url
         """
         load_dotenv()
-
-    def test_get_song(self):
-        """Test retrieving a song via the Genius API"""
-        song_id = 2979924
-        exp_song = {
+        self.song_id = 2979924
+        self.song = {
             'album': None,
             'apple_music_player_url': 'https://genius.com/songs/2979924/apple_music_player',
             'artist': 'Men I Trust',
@@ -29,27 +27,33 @@ class TestGenius(absltest.TestCase):
             'song_art_image_thumbnail_url': \
                 'https://images.genius.com/9a956e5a7c0d78e8441b31bdf14dc87b.300x300x1.jpg',
             'title': 'Lauren',
-            'url': 'https://genius.com/Men-i-trust-lauren-lyrics'}
-        with patch('utilities.genius.requests.get') as mocked_get:
-            mocked_get.return_value.status_code = 200
-            mocked_get.return_value.text = (
-                r'''{"response":{"song":{"apple_music_player_url":'''
-                r'''"https://genius.com/songs/2979924/apple_music_player",'''
-                r'''"embed_content":"<div id='rg_embed_link_2979924' '''
-                r'''class='rg_embed_link' data-song-id='2979924'>Read '''
-                r'''<a href='https://genius.com/Men-i-trust-lauren-lyrics'>'''
-                r'''“Lauren” by Men I Trust</a> on Genius</div> <script '''
-                r'''crossorigin src='//genius.com/songs/2979924/embed.js'>'''
-                r'''</script>","id":2979924,"song_art_image_thumbnail_url":'''
-                r'''"https://images.genius.com/9a956e5a7c0d78e8441b31bdf14dc87b'''
-                r'''.300x300x1.jpg","title":"Lauren","url":"https://genius.com'''
-                r'''/Men-i-trust-lauren-lyrics","album":null,"primary_artist":'''
-                r'''{"name":"Men I Trust"}}}}'''
-            )
-            song = genius.get_song(song_id)
-            mocked_get.asset_called_with(
-                'https://api.genius.com/songs/{song_id}'.format(song_id=song_id))
-            self.assertEqual(song, exp_song)
+            'url': 'https://genius.com/Men-i-trust-lauren-lyrics',
+        }
+        self.token = os.getenv('GENIUS_ACCESS_TOKEN')
+        self.headers = {'Authorization': 'Bearer {token}'.format(token=self.token)}
+        self.url = 'https://api.genius.com/songs/{song_id}'.format(song_id=self.song_id)
+
+    @patch.object(genius.requests, 'get')
+    def test_get_song(self, mock_get):
+        """Test retrieving a song via the Genius API"""
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.text = (
+            r'''{"response":{"song":{"apple_music_player_url":'''
+            r'''"https://genius.com/songs/2979924/apple_music_player",'''
+            r'''"embed_content":"<div id='rg_embed_link_2979924' '''
+            r'''class='rg_embed_link' data-song-id='2979924'>Read '''
+            r'''<a href='https://genius.com/Men-i-trust-lauren-lyrics'>'''
+            r'''“Lauren” by Men I Trust</a> on Genius</div> <script '''
+            r'''crossorigin src='//genius.com/songs/2979924/embed.js'>'''
+            r'''</script>","id":2979924,"song_art_image_thumbnail_url":'''
+            r'''"https://images.genius.com/9a956e5a7c0d78e8441b31bdf14dc87b'''
+            r'''.300x300x1.jpg","title":"Lauren","url":"https://genius.com'''
+            r'''/Men-i-trust-lauren-lyrics","album":null,"primary_artist":'''
+            r'''{"name":"Men I Trust"}}}}'''
+        )
+        song = genius.get_song(self.song_id)
+        mock_get.assert_called_with(url=self.url, headers=self.headers)
+        self.assertEqual(song, self.song)
 
 if __name__ == '__main__':
     absltest.main()
