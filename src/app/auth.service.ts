31064cdedd80502d1957777f467d393799cb9cb9
Henry Le <hle@raybeam.com>
2020-09-28 04:25:02 -0700

    Auth user if not logged in

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,6 +6,7 @@ import {flatMap, map} from 'rxjs/operators';
 
 import 'firebase/auth';
 import {auth} from 'firebase/app';
+import * as firebase from 'firebase';
 import {AngularFireAuth} from '@angular/fire/auth';
 
 import {Recommendation} from './models/recommendation.model';
@@ -16,12 +17,17 @@ import {environment} from '../environments/environment';
 })
 export class AuthService {
   readonly headers = new HttpHeaders({'Content-Type': 'application/json'});
+  user: Promise<firebase.User | null>;
 
   constructor(
     private http: HttpClient,
     private auth: AngularFireAuth,
     private window: Window
-  ) {}
+  ) {
+    this.user = new Promise((resolve, reject) => {
+      this.auth.onAuthStateChanged(user => resolve(user));
+    });
+  }
 
   /* TODO Simple validation/processing for now */
   /** Processes song recommendation. */
@@ -45,17 +51,21 @@ export class AuthService {
       );
   }
 
-  /** Authenticates a user and returns an Observable for their ID token. */
+  /** Authenticates a user, if not signed in, and returns an Observable for their ID token. */
   authenticateWithGoogle(): Observable<string> {
-    const authPromise = this.auth.signInWithPopup(
-      new auth.GoogleAuthProvider()
-    );
-    return from(authPromise).pipe(
-      flatMap(() => {
-        return this.auth.currentUser.then(user => {
-          if (user == null) throw new Error('No user found after signed in.');
-          return user.getIdToken(true);
-        });
+    return from(this.user).pipe(
+      flatMap(user => {
+        const authPromise = user
+          ? Promise.resolve()
+          : this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+        return from(authPromise).pipe(
+          flatMap(() => {
+            return this.auth.currentUser.then(user => {
+              if (user == null) throw new Error('No user signed in.');
+              return user.getIdToken();
+            });
+          })
+        );
       })
     );
   }
