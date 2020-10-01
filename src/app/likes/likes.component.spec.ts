ce855322d3f5718a568d22f9f8201f9c312bef12
Henry Le <hle@raybeam.com>
2020-09-30 21:09:55 -0700

    Updated LikesComponent tests

--- src/app/likes/likes.component.spec.ts
+++ src/app/likes/likes.component.spec.ts
@@ -10,6 +10,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 
+import {SafePipeModule} from 'safe-pipe';
 import {MaterialModule} from '../material/material.module';
 
 import {AngularFireModule} from '@angular/fire';
@@ -29,8 +30,36 @@ describe('LikesComponent', () => {
   const idToken: string = 'idToken';
 
   const likes: Like[] = [
-    {uid: 'uid1', id: 0},
-    {uid: 'uid1', id: 1},
+    {
+      album: 'Depression Cherry',
+      apple_music_player_url:
+        'https://genius.com/songs/1929412/apple_music_player',
+      artist: 'Beach House',
+      email: 'moot@gmail.com',
+      embed_content:
+        "<div id='rg_embed_link_1929412' class='rg_embed_link' data-song-id='1929412'>Read <a href='https://genius.com/Beach-house-space-song-lyrics'>“Space Song” by Beach House</a> on Genius</div> <script crossorigin src='//genius.com/songs/1929412/embed.js'></script>",
+      id: 1929412,
+      song_art_image_url:
+        'https://images.genius.com/98ce1842b01c032eef50b8726fbbfba6.900x900x1.jpg',
+      title: 'Space Song',
+      uid: 'u1d',
+      url: 'https://genius.com/Beach-house-space-song-lyrics',
+    },
+    {
+      album: 'Non-Album Single',
+      apple_music_player_url:
+        'https://genius.com/songs/2979924/apple_music_player',
+      artist: 'Men I Trust',
+      email: 'moot@gmail.com',
+      embed_content:
+        "<div id='rg_embed_link_2979924' class='rg_embed_link' data-song-id='2979924'>Read <a href='https://genius.com/Men-i-trust-lauren-lyrics'>“Lauren” by Men I Trust</a> on Genius</div> <script crossorigin src='//genius.com/songs/2979924/embed.js'></script>",
+      id: 2979924,
+      song_art_image_url:
+        'https://images.genius.com/9a956e5a7c0d78e8441b31bdf14dc87b.1000x1000x1.jpg',
+      title: 'Lauren',
+      uid: 'u1d',
+      url: 'https://genius.com/Men-i-trust-lauren-lyrics',
+    },
   ];
 
   beforeEach(async(() => {
@@ -49,6 +78,7 @@ describe('LikesComponent', () => {
       imports: [
         HttpClientTestingModule,
         MaterialModule,
+        SafePipeModule,
         AngularFireModule.initializeApp(environment.firebaseConfig),
       ],
       declarations: [LikesComponent],
@@ -78,7 +108,7 @@ describe('LikesComponent', () => {
   }));
 
   it('should display the liked songs', async(() => {
-    const likes = fixture.debugElement.queryAll(By.css('.mat-list'));
+    const likes = fixture.debugElement.queryAll(By.css('.mat-grid-list'));
     expect(likes).not.toEqual([]);
   }));
 });
