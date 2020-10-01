53ea035a38caaed2b69c4d1d31fec0d6be16762b
Henry Le <hle@raybeam.com>
2020-09-30 20:45:32 -0700

    Updated Like model, LikesComponent template, and styling

--- src/app/models/like.model.ts
+++ src/app/models/like.model.ts
@@ -1,4 +1,12 @@
 export interface Like {
+  album?: string;
+  apple_music_player_url?: string;
+  artist?: string;
+  email?: string;
+  embed_content?: string;
   id?: number;
+  song_art_image_url?: string;
+  title?: string;
   uid?: string;
+  url?: string;
 }
