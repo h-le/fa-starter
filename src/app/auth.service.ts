00322558ba2b179f6c4ca97d8283258f5397c885
Henry Le <hle@raybeam.com>
2020-10-02 15:02:43 -0700

    Separated auth/http services, including recommendation validation method

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,62 +1,23 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {Observable, from} from 'rxjs';
-import {flatMap, map} from 'rxjs/operators';
+import {flatMap} from 'rxjs/operators';
 
 import 'firebase/auth';
 import {auth} from 'firebase/app';
 import * as firebase from 'firebase';
 import {AngularFireAuth} from '@angular/fire/auth';
 
-import {Recommendation} from './models/recommendation.model';
-import {Like} from './models/like.model';
-import {environment} from '../environments/environment';
-
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  readonly headers = new HttpHeaders({'Content-Type': 'application/json'});
   user: Promise<firebase.User | null>;
 
-  constructor(
-    private http: HttpClient,
-    private auth: AngularFireAuth,
-    private window: Window
-  ) {
+  constructor(private auth: AngularFireAuth, private window: Window) {
     this.user = new Promise(resolve => {
       this.auth.onAuthStateChanged(user => resolve(user));
     });
-  }
-
-  /* TODO Simple validation/processing for now */
-  /** Processes song recommendation. */
-  validateRecommendation(song): Recommendation {
-    if (song.album == null) {
-      song.album = 'Non-Album Single';
-    }
-    return song;
-  }
-
-  /** Returns an Observable for the songs liked by the signed-in user. */
-  getLikes(idToken: string): Observable<Like[]> {
-    return this.http.get<Like[]>(environment.url + '_likes', {
-      headers: this.headers.append('Authorization', 'Bearer ' + idToken),
-    });
-  }
-
-  /** Returns an Observable for a song recommendation to the signed-in user. */
-  getRecommendation(idToken: string): Observable<Recommendation> {
-    return this.http
-      .get<Recommendation>(environment.url + '_recommend', {
-        headers: this.headers.append('Authorization', 'Bearer ' + idToken),
-      })
-      .pipe(
-        map((song: Recommendation) => {
-          return this.validateRecommendation(song);
-        })
-      );
   }
 
   /** Authenticates a user, if not signed in, and returns an Observable for their ID token. */
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 
 import {Observable} from 'rxjs';
-import {flatMap} from 'rxjs/operators';
+import {flatMap, map} from 'rxjs/operators';
 
 import {Recommendation} from '../models/recommendation.model';
 
 import {AuthService} from '../auth.service';
+import {HttpService} from '../http.service';
 
 @Component({
   selector: 'app-home',
@@ -13,13 +14,23 @@ import {AuthService} from '../auth.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  endpoint: string = '_recommend';
   recommendation$: Observable<Recommendation>;
 
-  constructor(public authService: AuthService) {
+  constructor(
+    public authService: AuthService,
+    public httpService: HttpService
+  ) {
     this.recommendation$ = authService.authenticateWithGoogle().pipe(
-      flatMap((idToken: string) => {
-        return authService.getRecommendation(idToken);
-      })
+      flatMap((idToken: string) =>
+        httpService.get<Recommendation>(idToken, this.endpoint)
+      ),
+      map((song: Recommendation) => this.validateRecommendation(song))
     );
+  }
+
+  /** Validates/processes song recommendation. */
+  validateRecommendation({album, ...rest}: Recommendation): Recommendation {
+    return {album: album ? album : 'Non-Album Single', ...rest};
   }
 }
