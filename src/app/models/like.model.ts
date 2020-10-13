cc2d7a30fd9a8d5239ba6eb04feaead3ac224cf9
Henry Le <hle@raybeam.com>
2020-10-13 14:58:25 -0700

    Added time_of_day field

--- src/app/models/like.model.ts
+++ src/app/models/like.model.ts
@@ -7,6 +7,7 @@ export interface Like {
   /** ID of song in Genius */
   id?: number;
   song_art_image_url?: string;
+  time_of_day?: string;
   title?: string;
   /** Firebase ID of user that liked this song */
   uid?: string;
